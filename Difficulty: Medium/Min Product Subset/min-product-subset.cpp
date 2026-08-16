3674480741380da388413e220a9b9f8f530a04d6
Anuj Kushwaha <anujkush2431393@gmail.com>
2026-08-16 15:10:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Min Product Subset/min-product-subset.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int minProd(vector<int>& arr) {
+        int n = arr.size();
+        int ans = INT_MAX;
+
+        // Generate all non-empty subsets
+        for (int mask = 1; mask < (1 << n); mask++) {
+            int product = 1;
+
+            for (int i = 0; i < n; i++) {
+                if (mask & (1 << i)) {
+                    product *= arr[i];
+                }
+            }
+
+            ans = min(ans, product);
+        }
+
+        return ans;
+    }
+};
