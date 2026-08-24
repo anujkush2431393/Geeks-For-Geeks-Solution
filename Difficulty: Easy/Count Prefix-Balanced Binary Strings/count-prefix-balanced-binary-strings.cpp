cf08277c87718831b8581901299510e974fe0177
Anuj Kushwaha <anujkush2431393@gmail.com>
2026-08-24 20:22:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Prefix-Balanced Binary Strings/count-prefix-balanced-binary-strings.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int prefixStrings(int n) {
+        const long long MOD = 1000000007;
+
+        vector<long long> dp(n + 1, 0);
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] = (dp[i] + dp[j] * dp[i - 1 - j]) % MOD;
+            }
+        }
+
+        return dp[n];
+    }
+};
