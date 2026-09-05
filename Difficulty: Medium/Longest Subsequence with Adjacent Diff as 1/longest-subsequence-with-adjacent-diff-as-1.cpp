33bf251481c75a6f125f80ea53fe15ac1e7eab4d
Anuj Kushwaha <anujkush2431393@gmail.com>
2026-09-05 20:10:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Subsequence with Adjacent Diff as 1/longest-subsequence-with-adjacent-diff-as-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int longestSubseq(vector<int>& arr) {
+        unordered_map<int, int> dp;
+        int ans = 1;
+
+        for (int x : arr) {
+            int prev = max(dp[x - 1], dp[x + 1]);
+
+            dp[x] = max(dp[x], prev + 1);
+
+            ans = max(ans, dp[x]);
+        }
+
+        return ans;
+    }
+};
